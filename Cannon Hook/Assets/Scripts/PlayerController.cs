d7327fcff4aa5de0370e1925571f2fd5cfd73975
anderstaylor <111925828+AndersTaylor@users.noreply.github.com>
2025-05-31 18:19:49 -0500

    modular player controller import

---
+++ Cannon Hook/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed = 5f;
+    public float jumpPower = 1f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
